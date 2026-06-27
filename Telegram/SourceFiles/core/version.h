cba08adf694c39af0234189a1e2c64b42a48f1c4
loop-uh <vorones2004@gmail.com>
2026-06-27 23:02:28 +0300

    Rebrand to ZaStoGram: app name and icon
    AppName, Windows app id (separate data dir / single-instance) and exe ProductName/FileDescription set to ZaStoGram. Replace app icons (icon16..512 +@2x, icon256.ico, logo_256) with the shot-glass icon. Keep AppFile=Telegram so the CI artifact step still finds Telegram.exe.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -20,7 +20,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 // used in Updater.cpp and Setup.iss for Windows
 constexpr auto AppId = "{53F49750-6209-4FBF-9CA8-7A333C87D1ED}"_cs;
 constexpr auto AppNameOld = "Telegram Win (Unofficial)"_cs;
-constexpr auto AppName = "Telegram Desktop"_cs;
+constexpr auto AppName = "ZaStoGram"_cs;
 constexpr auto AppFile = "Telegram"_cs;
 constexpr auto AppVersion = 6009003;
 constexpr auto AppVersionStr = "6.9.3";
